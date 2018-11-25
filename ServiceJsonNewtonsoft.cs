0d53ac3c04b82557afeae474b32b46c7a3ebd8b0
thZero.com <development@thzero.com>
2018-11-25 17:07:03 -0600

    Updates to generic services for both Factory as well as .NET Injection

--- ServiceJsonNewtonsoft.cs
+++ ServiceJsonNewtonsoft.cs
@@ -20,83 +20,154 @@ limitations under the License.
 using System;
 using System.IO;
 
+using Microsoft.Extensions.Logging;
+
 using Newtonsoft.Json;
 
 namespace thZero.Services
 {
-	public sealed class ServiceJsonNewtonsoft : IServiceJson
-	{
-		#region Public Methods
-		public T Deserialize<T>(string data)
-		{
-			Enforce.AgainstNullOrEmpty(() => data);
+    public sealed class ServiceJsonNewtonsoftFactory : Internal.ServiceJsonNewtonsoftBase<ServiceJsonNewtonsoftFactory>, IServiceJson
+    {
+        private static readonly thZero.Services.IServiceLog log = thZero.Factory.Instance.RetrieveLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-			return JsonConvert.DeserializeObject<T>(data);
-		}
+        public ServiceJsonNewtonsoftFactory() : base(log, null)
+        {
+        }
+    }
 
-		public object Deserialize(string data, Type type)
-		{
-			Enforce.AgainstNullOrEmpty(() => data);
-			Enforce.AgainstNull(() => type);
+    public sealed class ServiceJsonNewtonsoft : ServiceLoggableBase<ServiceJsonNewtonsoft>, IServiceJson
+    {
+        public ServiceJsonNewtonsoft(ILogger<ServiceJsonNewtonsoft> logger) : base(logger)
+        {
+            _instance = new Internal.ServiceJsonNewtonsoftBase<ServiceJsonNewtonsoft>(null, logger);
+        }
 
-			return JsonConvert.DeserializeObject(data, type);
-		}
+        #region Public Methods
+        public T Deserialize<T>(string data)
+        {
+            return _instance.Deserialize<T>(data);
+        }
 
-		public T Deserialize<T>(Stream stream)
-		{
-			throw new NotImplementedException();
-		}
+        public object Deserialize(string data, Type type)
+        {
+            return _instance.Deserialize(data, type);
+        }
 
-		public object Deserialize(Stream stream, Type type)
-		{
-			throw new NotImplementedException();
-		}
+        public T Deserialize<T>(Stream stream)
+        {
+            return _instance.Deserialize<T>(stream);
+        }
 
-		public object DeserializeObject(string data)
-		{
-			return JsonConvert.DeserializeObject(data);
-		}
+        public object Deserialize(Stream stream, Type type)
+        {
+            return _instance.Deserialize(stream, type);
+        }
 
-		public string Serialize(object data)
-		{
-			return JsonConvert.SerializeObject(data);
-		}
+        public object DeserializeObject(string data)
+        {
+            return _instance.DeserializeObject(data);
+        }
 
-		public void Serialize(object data, Stream stream)
-		{
-			throw new NotImplementedException();
-		}
-		#endregion
+        public string Serialize(object data)
+        {
+            return _instance.Serialize(data);
+        }
 
-		#region Public Properties
-		public IServiceJsonSettings Settings
-		{
-			get
-			{
-				if (_settingsJson != null)
-					return _settingsJson;
+        public void Serialize(object data, Stream stream)
+        {
+            _instance.Serialize(data, stream);
+        }
+        #endregion
 
-				lock (_lock)
-				{
-					if (_settingsJson != null)
-						return _settingsJson;
+        #region Public Properties
+        public IServiceJsonSettings Settings => _instance.Settings;
+        #endregion
 
-					_settingsJson = new ServiceJsonSettings
-					{
-						ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
-						MissingMemberHandling = Newtonsoft.Json.MissingMemberHandling.Ignore,
-						NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-						DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.IgnoreAndPopulate
-					};
-					return _settingsJson;
-				}
-			}
-		}
-		#endregion
+        #region Fields
+        private static Internal.ServiceJsonNewtonsoftBase<ServiceJsonNewtonsoft> _instance;
+        #endregion
+    }
+}
 
-		#region Fields
-		private static readonly object _lock = new object();
-		private static volatile IServiceJsonSettings _settingsJson;
-		#endregion
-	}
+namespace thZero.Services.Internal
+{
+    public class ServiceJsonNewtonsoftBase<TService> : IntermediaryServiceBase<TService>
+    {
+        public ServiceJsonNewtonsoftBase(thZero.Services.IServiceLog log, ILogger<TService> logger) : base(log, logger)
+        {
+        }
+
+        #region Public Methods
+        public T Deserialize<T>(string data)
+        {
+            Enforce.AgainstNullOrEmpty(() => data);
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public object Deserialize(string data, Type type)
+        {
+            Enforce.AgainstNullOrEmpty(() => data);
+            Enforce.AgainstNull(() => type);
+
+            return JsonConvert.DeserializeObject(data, type);
+        }
+
+        public T Deserialize<T>(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Deserialize(Stream stream, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object DeserializeObject(string data)
+        {
+            return JsonConvert.DeserializeObject(data);
+        }
+
+        public string Serialize(object data)
+        {
+            return JsonConvert.SerializeObject(data);
+        }
+
+        public void Serialize(object data, Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Public Properties
+        public IServiceJsonSettings Settings
+        {
+            get
+            {
+                if (_settingsJson != null)
+                    return _settingsJson;
+
+                lock (_lock)
+                {
+                    if (_settingsJson != null)
+                        return _settingsJson;
+
+                    _settingsJson = new ServiceJsonSettings
+                    {
+                        ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver(),
+                        MissingMemberHandling = Newtonsoft.Json.MissingMemberHandling.Ignore,
+                        NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+                        DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.IgnoreAndPopulate
+                    };
+                    return _settingsJson;
+                }
+            }
+        }
+        #endregion
+
+        #region Fields
+        private static readonly object _lock = new object();
+        private static volatile IServiceJsonSettings _settingsJson;
+        #endregion
+    }
 }
